fc534ff6bcb804c7d9f2e063b58e93b6b77a4ef8
Simona <simona.liu@mail.utoronto.ca>
2026-09-17 15:03:32 -0400

    made the two functions take in integers only

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 class utils:
     @staticmethod
     def reversed(number: int) -> int:
+        if not isinstance(number, int):
+            raise TypeError
         return int(str(number)[::-1])
 
     @staticmethod
     def formatter(number: int):
+        if not isinstance(number, int):
+            raise TypeError
         return bin(number), oct(number)
